6dfc119f5712af56d2a23d6a2c7aac50c31df955
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-07-23 22:05:38 +0300

    add UserSerializer

--- bikes/rental/views.py
+++ bikes/rental/views.py
@@ -3,6 +3,20 @@ from bikes.rental.serializers import BikesSerializer
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from django.contrib.auth.models import User
+from rest_framework import generics
+from bikes.rental.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 
 
 class BikesList(APIView):
--- bikes/settings.py
+++ bikes/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bikes.rental',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
