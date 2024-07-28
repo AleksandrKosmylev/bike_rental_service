147f6d57602d85948a587b76306e38da31f6152c
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-07-28 11:21:55 +0300

    register models in admin

--- bikes/rental/admin.py
+++ bikes/rental/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from bikes.rental.models import Bikes
+from bikes.users.models import CustomUser
+
 
 # Register your models here.
+admin.site.register(Bikes)
+admin.site.register(CustomUser)
